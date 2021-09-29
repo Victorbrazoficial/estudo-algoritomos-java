027166fd8225db0e3f18161e815c259a3232ac56
Victor Braz <Vic_braz@outlook.com>
2021-09-29 18:31:12 -0300

    Estudo Refatoração usando for (Semáforo Temporizador) Inclusão de comentarios

--- src/estrutura/repeticao/SemaforoRefatoradoUsandoFor.java
+++ src/estrutura/repeticao/SemaforoRefatoradoUsandoFor.java
@@ -1,24 +1,28 @@
 package estrutura.repeticao;
 
+// Semaforo Temporizador refatorado "commit : Estudo Estrutura de Repetição (Semaforo com Temporizado)"
 public class SemaforoRefatoradoUsandoFor {
 
     public static void main(String[] args) throws InterruptedException 
     {
-        
-        while (true) 
-        {    
-                System.out.println("|SEMÁFORO VERDE|");
-                Thread.sleep(1000);
 
-            for (int semaforoVerde = 5; semaforoVerde >= 0; semaforoVerde--)
+// As variaveis foram inseridas em cada "for" visando evitar desperdicio de memoria enquanto ela não esta sendo ultilizada.
+
+        while (true) // loop infinito.
+        {    
+                System.out.println("|SEMÁFORO VERDE|"); // Print reduzido.
+                Thread.sleep(1000); // Quebra de um segundo para manter a sintonia do print junto com os "for's"
+
+            for (int semaforoVerde = 5; semaforoVerde >= 0; semaforoVerde--) // Troca de while para "for".
             {
                 System.out.println(semaforoVerde + " Segundos");
-                Thread.sleep(1000);
+                Thread.sleep(1000); // Quebra de Segundo continua igual.
             }  
 
                 System.out.println("|SEMÁFORO AMARELO|");
                 Thread.sleep(1000);
-            for (int semaforoAmarelo = 2; semaforoAmarelo >= 0; semaforoAmarelo--)
+
+            for (int semaforoAmarelo = 2; semaforoAmarelo >= 0; semaforoAmarelo--) // Trocado de while para "for".
             {
                 System.out.println(semaforoAmarelo + " Segundos");
                 Thread.sleep(1000);
@@ -26,7 +30,8 @@ public class SemaforoRefatoradoUsandoFor {
 
                 System.out.println("|SEMÁFORO VERMELHO|");
                 Thread.sleep(1000);
-            for (int semaforoVermelho = 3; semaforoVermelho >=0; semaforoVermelho--)    
+
+            for (int semaforoVermelho = 3; semaforoVermelho >=0; semaforoVermelho--) // Trocado de while para "for".   
             {
                 System.out.println(semaforoVermelho + " Segundos");
                 Thread.sleep(1000);
