1ba45506eb650b3cfba86efa2633e8e5a2fd66d7
Victor Braz <Vic_braz@outlook.com>
2021-10-01 12:41:58 -0300

    Estudo sobre Procedimento (Sem parametro/Retorno)

---
+++ src/procedimento/ProcedimentoSemParamentroESemRetorno.java
@@ -0,0 +1,37 @@
+package procedimento;
+
+import java.util.Scanner;
+
+// Rotina sem passagem de paramentro e sem retorno.
+public class ProcedimentoSemParamentroESemRetorno {
+
+    public static int numeroUsuario; // Variavel Global
+    
+    public static void entradaUsuario() // Procedimento entrada do usuario.
+    {
+        Scanner entrada = new Scanner(System.in);
+            System.out.println("Digite um numero: ");
+        numeroUsuario = entrada.nextInt();
+            System.out.println("numero é: " + numeroUsuario);
+    }    
+
+    public static void ParOuImpar() // Procedimento compara Par ou impar depois da entrada do usuario.
+    {
+        entradaUsuario(); // Procedimento entrada do usuario.
+
+        if (numeroUsuario % 2 == 0) // Se resto for igual a 0, printa "PAR"
+        {
+            System.out.println("O numero " + numeroUsuario + " é PAR.");
+        }
+        else // Se não for igual a zero o resto, printa Impar.
+        {
+            System.out.println("O numero " + numeroUsuario + " é IMPAR.");
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        ParOuImpar(); // Inclui os dois procedimento: entradaUsuario() e parouImpar().
+    }
+}
+    
