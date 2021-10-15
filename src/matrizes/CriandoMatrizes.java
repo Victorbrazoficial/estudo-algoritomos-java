0364897f5798ffb522f24e32e6988f0d191b8bb7
Victor Braz <Vic_braz@outlook.com>
2021-10-15 11:00:30 -0300

    Estudo sobre Matrizes (Busca de posições dentro da matriz)

--- src/matrizes/CriandoMatrizes.java
+++ src/matrizes/CriandoMatrizes.java
@@ -7,16 +7,16 @@ public class CriandoMatrizes
 {
     public static void main(String[] args) 
     {
-        int matriz[][] = new int[2][2];
+        int matriz[][] = new int[4][2];
         int linha = 0, coluna = 0;
 
         Scanner entrada = new Scanner(System.in);
         
-        for (linha = 0; linha < matriz.length; linha++) // Contador das linhas    
+        for (linha = 0; linha <= 3; linha++) // Contador das linhas    
         {         
             System.out.println("Insira os valores da LINHA " + linha);
             
-        for (coluna = 0; coluna < matriz.length; coluna++) // contador das colunas
+        for (coluna = 0; coluna <= 1; coluna++) // contador das colunas
         {            
             matriz[linha][coluna] = entrada.nextInt(); // Entrada do usuario.        
             System.out.println("entrada LINHA " + linha + " COLUNA " + coluna + " = " + matriz[linha][coluna]);
@@ -26,9 +26,22 @@ public class CriandoMatrizes
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("VALORES INSERIDOS PELO USUARIO");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("matriz[0][0] = "+ matriz[0][0]);
+        System.out.println("matriz[0][0] = "+matriz[0][0]);
         System.out.println("matriz[0][1] = "+matriz[0][1]);
         System.out.println("matriz[1][0] = "+matriz[1][0]);
-        System.out.println("matriz[1][1] = "+matriz[1][1]);
+        System.out.println("matriz[1][1] = "+matriz[1][1]); 
+        System.out.println("matriz[2][0] = "+matriz[2][0]);
+        System.out.println("matriz[2][1] = "+matriz[2][1]);
+        System.out.println("matriz[3][0] = "+matriz[3][0]);
+        System.out.println("matriz[3][1] = "+matriz[3][1]); 
+
+        System.out.println("---------------------------------------");
+        System.out.println("BUSCA DE TODAS AS POSIÇÕES DA COLUNA 0");
+        System.out.println("---------------------------------------");
+        
+        for (int i = 0; i <= 3; i++)
+        {
+            System.out.println("" + matriz[i][0]);
+        }
     }
 }
